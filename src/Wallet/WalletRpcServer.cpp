02486fc0173cc6c1d6677f358ec50ed7f8558a28
aiwe <maksymivv@gmail.com>
2021-11-01 10:34:04 +0200

    Add extra to simplewallet RPC transfer command
    It should be correctly formatted and encoded

--- src/Wallet/WalletRpcServer.cpp
+++ src/Wallet/WalletRpcServer.cpp
@@ -295,9 +295,17 @@ bool wallet_rpc_server::on_transfer(const wallet_rpc::COMMAND_RPC_TRANSFER::requ
         "Something went wrong with payment_id. Please check its format: " + payment_id_str + ", expected 64-character string");
     }
   }
+  else if (!req.extra.empty()) {
+    std::string extra_str = req.extra;
+    if (!Common::fromHex(req.extra, extra)) {
+      throw JsonRpc::JsonRpcError(WALLET_RPC_ERROR_CODE_WRONG_EXTRA,
+        "Wrong transaction extra format: " + extra_str + ", expected 64-character string");
+    }
+  }
 
   std::string extraString;
   std::copy(extra.begin(), extra.end(), std::back_inserter(extraString));
+
   try
   {
     CryptoNote::WalletHelper::SendCompleteResultObserver sent;
