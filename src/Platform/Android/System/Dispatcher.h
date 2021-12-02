53b924d8cd4c11436a33fd5427140a0e5baacba8
Aiwe <maksymivv@gmail.com>
2021-12-02 12:32:57 +0200

    Try to define SIZEOF_PTHREAD_MUTEX_T to see if it causes segmentation fault

--- src/Platform/Android/System/Dispatcher.h
+++ src/Platform/Android/System/Dispatcher.h
@@ -84,6 +84,7 @@ public:
   int getTimer();
   void pushTimer(int timer);
 
+/*
 #ifdef __x86_64__
 # if __WORDSIZE == 64
   static const int SIZEOF_PTHREAD_MUTEX_T = 40;
@@ -95,6 +96,8 @@ public:
 #else
   static const int SIZEOF_PTHREAD_MUTEX_T = 24;
 #endif
+*/
+static const int SIZEOF_PTHREAD_MUTEX_T = sizeof(pthread_mutex_t);
 
 private:
   void spawn(std::function<void()>&& procedure);
