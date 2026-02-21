fd361f31ff08fc3515ebf1c54ab6cb03c1b3bf8e
aiwe <maksymivv@gmail.com>
2026-02-21 13:52:17 -0600

    Anchor peer list should be ephemeral, don't store it in config

--- src/P2p/PeerListManager.cpp
+++ src/P2p/PeerListManager.cpp
@@ -63,7 +63,7 @@ PeerlistManager::Peerlist::Peerlist(peers_indexed& peers, size_t maxSize) :
 }
 
 void PeerlistManager::serialize(ISerializer& s) {
-  const uint8_t currentVersion = 2;
+  const uint8_t currentVersion = 3;
   uint8_t version = currentVersion;
 
   s(version, "version");
@@ -74,7 +74,6 @@ void PeerlistManager::serialize(ISerializer& s) {
 
   s(m_peers_white, "whitelist");
   s(m_peers_gray, "graylist");
-  s(m_peers_anchor, "anchorlist");
 }
 
 size_t PeerlistManager::Peerlist::count() const {
